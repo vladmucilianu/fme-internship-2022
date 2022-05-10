6a712adba7000af8af9cfff47a23a520d366e3b8
Robert Deak <r.deak@fme.ro>
2022-05-10 12:37:24 +0300

    Added text descriptions.

--- test-internship-p2/src/fme/internship/Test2.java
+++ test-internship-p2/src/fme/internship/Test2.java
@@ -1,5 +1,37 @@
+/*______________________________________________________________________________________________________
+ * 
+ * Write a Java program that reads text from a file and prints out the third most frequent word(s)
+ * in the text. If there is more than one word they will all be printed. 
+ * 
+ * The text contains only alpha-numerical characters and spaces, 
+ * and words are delimited by one or more spaces.
+ * _____________________________________________________________________________________________________
+ * 
+ * 
+ * Example:
+ * 
+ *______________________________________________________________________________________________________
+ *
+ *  Input: test.in
+ *      
+ *       test1 test2 test3 test1 test test2 test2
+ *       test1 test3 test1 test test0
+ *       
+ *  Output:
+ *      test3, test
+ *______________________________________________________________________________________________________
+ *
+ *______________________________________________________________________________________________________ 
+ */
+
 package fme.internship;
 
+/**
+ * 
+ * @author <full name>
+ * @email <email address>
+ * 
+ */
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("FME Internship - Test2");
--- test-internship-p3/src/fme/internship/Test3.java
+++ test-internship-p3/src/fme/internship/Test3.java
@@ -1,5 +1,88 @@
+/*______________________________________________________________________________________________________
+ * 
+ * Write a Java program that reads a file containing on each line a geometric shape and its properties
+ * in the following format:
+ * 
+ * Name : property1=value, property2=value, …
+ * 
+ * Shapes are limited to Circle, Square, Rectangle, Triangle.
+ * 
+ * Create 2 files area.out and perimeter.out, respectively, and write all the shapes
+ * 
+ *  - In area.out: in descending order by their area, in the format:
+ *      Name: { property1=value, property2=value, ... , area = value }
+ *      
+ *  - In perimeter.out: in ascending order by their perimeter/circumference, in the format:
+ *      Name: { property1=value, property2=value, ... , area = value }
+ * 
+ * Formulas:
+ * 
+ *  |===========|===============================|=======================|
+ *  |Shape      |Area                           |Perimeter/Circumference|
+ *  |===========|===============================|=======================|
+ *  |Circle     |PI*r^2                         |2*PI*r                 |
+ *  |===========|===============================|=======================|
+ *  |Square     |length^2                       |4*length               |
+ *  |===========|===============================|=======================|
+ *  |Rectangle  |length*width                   |2*(length+width)       |         
+ *  |===========|===============================|=======================|
+ *  |Triangle   |sqrt(s*(s-a)*(s-b)*s(s-c))     |a+b+c                  |
+ *  |           |   where s = (a+b+c)/2         |                       |
+ *  |===========|===============================|=======================|
+ * _____________________________________________________________________________________________________
+ * 
+ * 
+ * Example:
+ * 
+ *______________________________________________________________________________________________________
+ *
+ *  Input: 
+ *  
+ *  shapes.in
+ *      
+ *      Circle : r=2
+ *      Square : length=4
+ *      Rectangle : length=3, width=4
+ *      Circle : r=3
+ *      Square : length=5
+ *      Square : length=3
+ *      Triangle: a=3, b=4, c=5
+ *       
+ *  Output:
+ *  
+ *  area.out
+ *      
+ *      Circle: { r=3, area=28.27 }
+ *      Square: { length=5, area=25 }
+ *      Square: { length=4, area=16 }
+ *      Circle: { r=2, area=12.56 }
+ *      Rectangle: { length=3, width=4, area=12 }
+ *      Square: { length=3, area=9 }
+ *      Triangle: { a=3, b=4, c=5, area=6 }
+ *      
+ *  perimeter.out
+ *  
+ *      Square: { length=3, perimeter=12 }
+ *      Triangle: { a=3, b=4, c=5, perimeter=12 }
+ *      Circle: { r=2, perimeter=12.56 }
+ *      Rectangle: { length=3, width=4, perimeter=14 }
+ *      Square: { length=4, perimeter=16 }
+ *      Circle: { r=3, perimeter=18.84 }
+ *      Square: { length=4, perimeter=20 }   
+ *       
+ *______________________________________________________________________________________________________
+ *
+ *______________________________________________________________________________________________________ 
+ */
+
 package fme.internship;
 
+/**
+ * 
+ * @author <full name>
+ * @email <email address>
+ * 
+ */
 public class Test3 {
 
     public static void main(String[] args) {
