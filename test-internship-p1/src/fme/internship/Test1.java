d5f2c7b4ba2c12f575e805b37da03b15495733d9
vladmucilianu <Mucilianu>
2022-05-20 13:57:45 +0300

    Solution

--- test-internship-p1/src/fme/internship/Test1.java
+++ test-internship-p1/src/fme/internship/Test1.java
@@ -28,16 +28,42 @@
 
 package fme.internship;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author <full name>
  * @email <email address>
  * 
  */
+
 public class Test1 {
-
-    public static void main(String[] args) {
-        System.out.println("FME Internship - Test1");
+    static int checkAscending(int[] arr, int n) {
+            if (n == 1 || n == 0)
+                return 1;
+            if (arr[n - 1] < arr[n - 2])
+                return 0;
+        return checkAscending(arr, n - 1);
     }
-
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of elements ");
+        n=sc.nextInt();
+        int[] arr = new int[10];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n; i++) {
+            arr[i]=sc.nextInt();
+            }
+        System.out.println("Array elements are: ");
+        for (int i=0; i<n; i++) {
+            System.out.print(arr[i]+" ");
+            }
+        System.out.println();
+        if (checkAscending(arr, n) != 0)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
 }
